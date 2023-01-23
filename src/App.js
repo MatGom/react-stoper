5838d247ed90f5355a4c93a0fdc9f68217500d0e
Mateusz Gomolka <mdm.gomolka@gmail.com>
2023-01-23 22:01:45 +0000

    Add Button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Container from './components/Container/Container';
 import Timer from './components/Timer/Timer';
+import Button from './components/Button/Button';
 
 function App() {
   return (
     <Container>
       <Timer />
+      <Button>Start</Button>
+      <Button>Stop</Button>
+      <Button>Reset</Button>
     </Container>
   );
 }
